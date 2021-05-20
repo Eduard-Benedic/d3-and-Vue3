8a2e9504e135fea62d412df4cb9a5402145c7d07
Eduard Benedic <benediceduard@outlook.com>
2021-05-20 09:20:11 +0100

    barchart added

--- src/components/Graphics/Barchart.vue
+++ src/components/Graphics/Barchart.vue
@@ -1,19 +1,74 @@
 <template>
   <div>
-    {{data}}
+    <h2>{{title}}</h2>
+    <svg :width="size[0]" :height="size[1]" ref="svgRef">
+      
+    </svg>
   </div>
+
 </template>
 
 <script>
-// import d3 from 'd3'
+import { ref, onMounted } from 'vue'
+
+import { select } from 'd3-selection'
+import { scaleLinear } from 'd3-scale'
+import { max } from 'd3-array'
+
 
 export default {
   props: {
-    data: Array
+    data: Array,
+    size: Array,
+    title: String
   },
-  setup() {
+  beforeMount() {
+    
+  },
+  beforeUnmount() {
 
+  },
+  setup(props) {
+    const svgRef = ref(null)
+    console.log(props)
+
+
+
+    onMounted(() => {
+        createBarChart(svgRef.value, props.data, props.size)
+    })
+    return {
+      svgRef
+    }
   }
+}
+
+function createBarChart (node, data, size) {
+
+
+  const dataMax = max(data)
+  const yScale = scaleLinear().domain([0, dataMax]).range([0, size[1]])
+
+  select(node)
+    .selectAll('rect')
+    .data(data)
+    .enter()
+    .append('rect')
+  
+  select(node)
+    .selectAll('rect')
+    .data(data)
+    .exit()
+    .remove()
+
+  select(node)
+    .selectAll('rect')
+    .data(data)
+    .style('fill', '#fe9922')
+    .attr('x', (d, i) => i * 25)
+    .attr('y', d => 600 - yScale(d))
+    .attr('height', d => yScale(d))
+    .attr('width', 25)
 }
 
 </script>
--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <!-- <Barchar :data=""/> -->
-    <Barchart :data="getUnivariateData" />
+    <div>
+        <label for="bar-type" class="block text-sm font-medium text-gray-700">Create chart based on:</label>
+        <select @change="changeBarchartData" id="bar-type" name="bar-type" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+          <option value="spend">Spend</option>
+          <option value="attributedROAS" selected="">Attributed ROAS</option>
+          <option value="attributedRevenue">Platform Revenue</option>
+          <option value="platformConversions">Platform Conversions</option>
+        </select>
+    </div>
+    <Barchart :title="'Chart based on something'" :size="[600, 600]" :data="getUnivariateData" />
   </div>
 </template>
 
@@ -19,7 +28,11 @@ export default {
 
     const store = useStore()
 
+    const changeBarchartData = (e) => {
+      console.log(e.target.value)
+    } 
     return {
+      changeBarchartData,
       getUnivariateData: computed(() => store.getters['campaign/getUnivariateData']('attributedRevenue'))
     }
 
